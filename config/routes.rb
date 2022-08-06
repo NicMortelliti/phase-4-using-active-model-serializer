4b63676fdd96b38bed4e07c2c132b4e967bffa0f
NicMortelliti <npmortelliti@gmail.com>
2022-08-06 11:25:26 -0700

    Move summary to its own serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+
+  # Return summary of one movie
+  get '/movies/:id/summary', to: 'movies#summary'
+
+  # Return summaries of all movies
+  get '/movies_summaries', to: 'movies#summaries'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
